29e74959d31c86353c23bc38a3fa1a07f7012722
bvidhyaprakash <vidhyaprakash.document@gmail.com>
2024-10-09 01:09:51 +0530

    latest push

--- src/main/java/vidhyaPrakashAutomation/ExtentResort/ExtentReportTestNG.java
+++ src/main/java/vidhyaPrakashAutomation/ExtentResort/ExtentReportTestNG.java
@@ -5,11 +5,9 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReportTestNG {
 
-	
-	public static ExtentReports getReportObjest() 
-	{
-		//String reportPath = System.getProperty("user.dir")+"\\reports\\index.html";
-		String reportPath = System.getProperty("user.dir")+"\\reports\\index.html";
+	public static ExtentReports getReportObjest() {
+		// String reportPath = System.getProperty("user.dir")+"\\reports\\index.html";
+		String reportPath = System.getProperty("user.dir") + "\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
 		reporter.config().setReportName("Amazon Test Report");
 		reporter.config().setDocumentTitle("Test Results");
